2b26c4b5cb0e6c2f0fc151b61a44b99a39acc602
asly-97 <abdeljabbar.elassali@gmail.com>
2022-10-30 20:56:38 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# chat_app
+
+A powerful chat flutter app
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:chat_app/screens/welcome_screen.dart';
+import 'package:chat_app/screens/login_screen.dart';
+import 'package:chat_app/screens/registration_screen.dart';
+import 'package:chat_app/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
