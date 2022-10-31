8dabb781636620ddf70aff52aa213cda642ed212
asly-97 <abdeljabbar.elassali@gmail.com>
2022-10-31 22:00:22 +0100

    Custom Flutter Animations

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -9,10 +9,48 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with TickerProviderStateMixin {
+  late AnimationController imgCtr;
+
+  late AnimationController txtCtr;
+  late Animation txtAnim;
+
+  late AnimationController bgCtr;
+  late Animation bgAnim;
+
+  @override
+  void initState() {
+    super.initState();
+
+    //Animation for Logo image
+    imgCtr = AnimationController(duration: Duration(seconds: 2), vsync: this);
+    imgCtr.forward();
+    imgCtr.addListener(() => setState(() {}));
+
+    //Animation for text 'Chat App'
+    txtCtr = AnimationController(vsync: this, duration: Duration(seconds: 2));
+    txtCtr.addListener(() => setState(() {}));
+    txtCtr.forward();
+    txtAnim = CurvedAnimation(parent: txtCtr, curve: Curves.bounceOut);
+
+    //Animation for background color
+    bgCtr = AnimationController(duration: Duration(seconds: 1), vsync: this);
+    bgCtr.addListener(() => setState(() {}));
+    bgCtr.forward();
+    bgAnim = ColorTween(begin: background3, end: background1).animate(bgCtr);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    imgCtr.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: bgAnim.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -28,7 +66,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                       padding: const EdgeInsets.all(8.0),
                       child: Image.asset(
                         'images/logo.png',
-                        color: accent,
+                        color: accent.withOpacity(imgCtr.value),
                       ),
                     ),
                     height: 60.0,
@@ -37,7 +75,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 Text(
                   'Chat App',
                   style: TextStyle(
-                      fontSize: 45.0,
+                      fontSize: txtAnim.value * 45.0,
                       fontWeight: FontWeight.w900,
                       color: text1),
                 ),
